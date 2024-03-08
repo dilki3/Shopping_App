ae5874194848594880f550bee4a386a00beda52f
dilki3 <dilkidinesha345@gmail.com>
2024-03-08 22:24:26 +0530

    Initial Commit

---
+++ Shopping_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shopping_App
+//
+//  Created by Dila Dinesha on 2024-03-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shopping_App/Shopping_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_AppApp.swift
+//  Shopping_App
+//
+//  Created by Dila Dinesha on 2024-03-08.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
